e8078a387297a62782bbcb6bcec4f3f4524e0d81
Tina Rsl <rasoloarison@yahoo.fr>
2019-12-16 00:20:29 +0100

    fisrt srv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { ProductService } from './shared/product.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { ProductComponent } from './product/product.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
